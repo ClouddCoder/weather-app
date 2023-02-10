99cee8f007478cf89e70d0b37a17ecec86eeabf8
Brayan Sanchez <brayan.sanchez.leon@correounivalle.edu.co>
2023-02-09 19:26:59 -0500

    Install tailwind to create a loader spinner when the API call is loading

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@ function App() {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState("");
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleGetWeather = async (e) => {
     e.preventDefault();
+
+    setLoading(true);
 
     try {
       const data = await getWeather(city);
@@ -16,6 +19,8 @@ function App() {
     } catch (err) {
       setError("City not found");
     }
+
+    setLoading(false);
   };
 
   const handleChange = (e) => {
@@ -25,15 +30,28 @@ function App() {
   return (
     <div className="container">
       <main>
-        <h1>Weather App</h1>
+        <h1 className="main__title">Weather App</h1>
         <form onSubmit={handleGetWeather} className="container-form">
           <section className="container-search">
             <input type="text" placeholder="City" onChange={handleChange} value={city} />
-            <button type="submit">Search</button>
+            {loading ? (
+              <button className="submit-button disabled" type="submit">
+                <div
+                  className="spinner-border animate-spin inline-block w-6 h-6 border-4 rounded-full text-loader"
+                  role="status"
+                >
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              </button>
+            ) : (
+              <button className="submit-button" type="submit">
+                Search
+              </button>
+            )}
           </section>
           <section className="container-weather">
             <section>
-              <h3>{weather ? weather.name : "-"}</h3>
+              <h3 className="main-subheading">{weather ? weather.name : "-"}</h3>
             </section>
             <section>
               <span>{weather ? weather.weather?.[0].description : "-"}</span>
@@ -51,15 +69,15 @@ function App() {
             </section>
             <section className="container-details">
               <div>
-                <h3>Feels like</h3>
+                <h3 className="main-subheading">Feels like</h3>
                 <span>{weather ? `${weather.main?.feels_like}°C` : "°C"}</span>
               </div>
               <div>
-                <h3>Humidity</h3>
-                <span>{weather ? `${weather.main?.humidity} %` : "%"}</span>
+                <h3 className="main-subheading">Humidity</h3>
+                <span>{weather ? `${weather.main?.humidity}%` : "%"}</span>
               </div>
               <div>
-                <h3>Pressure</h3>
+                <h3 className="main-subheading">Pressure</h3>
                 <span>{weather ? `${weather.main?.pressure} hPs` : "hps"}</span>
               </div>
             </section>
